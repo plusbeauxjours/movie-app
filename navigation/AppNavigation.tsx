2d11d0d3718c396a769a6be1f7f048c54dc7e7fc
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-19 12:48:18 +0900

    headerStyles

--- navigation/AppNavigation.tsx
+++ navigation/AppNavigation.tsx
@@ -4,21 +4,17 @@ import DetailScreen from "../screens/DetailScreen";
 import { HeaderStyles } from "../config";
 import { createAppContainer } from "react-navigation";
 
-const AppNavigation = createStackNavigator(
-  {
-    Main: {
-      screen: MainTabNavigator,
-      navigationOptions: { header: null }
-    },
-    Detail: {
-      screen: DetailScreen
-    }
+const AppNavigation = createStackNavigator({
+  Main: {
+    screen: MainTabNavigator,
+    navigationOptions: { header: null }
   },
-  {
+  Detail: {
+    screen: DetailScreen,
     navigationOptions: {
       ...HeaderStyles
     }
   }
-);
+});
 
 export default createAppContainer(AppNavigation);
